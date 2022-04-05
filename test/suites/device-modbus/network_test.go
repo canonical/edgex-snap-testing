aebb3806a4be946057a5122b7f4ac064de6ef90b
Mengyi Wang <mengyi.wang@canonical.com>
2022-04-05 16:57:21 +0200

    Add initial device-modbus test suite

---
+++ test/suites/device-modbus/network_test.go
@@ -0,0 +1,29 @@
+package test
+
+import (
+	"edgex-snap-testing/test/utils"
+	"testing"
+)
+
+const defaultServicePort = "59901"
+
+func TestNetworkInterface(t *testing.T) {
+	t.Cleanup(func() {
+		utils.SnapStop(t, deviceModbusService)
+	})
+
+	utils.SnapStart(t, deviceModbusService)
+
+	t.Run("listen default port "+defaultServicePort, func(t *testing.T) {
+		utils.WaitServiceOnline(t, defaultServicePort)
+	})
+
+	t.Run("not listen on all interfaces", func(t *testing.T) {
+		utils.RequireListenAllInterfaces(t, false, defaultServicePort)
+	})
+
+	t.Run("listen localhost", func(t *testing.T) {
+		utils.RequireListenLoopback(t, defaultServicePort)
+		utils.RequirePortOpen(t, defaultServicePort)
+	})
+}
