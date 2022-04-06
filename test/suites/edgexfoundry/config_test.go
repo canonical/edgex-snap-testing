37d3df6e41f55db5925de50b810f28fe3e0ae303
Mengyi <mengyi.wang@canonical.com>
2022-04-06 13:30:31 +0200

    Edgex 417 create edgexfoundry snap testing suite (#36)
    * Add CI workflows
    
    * Make configure.go works for both device/app and edgex-go services
    
    * Add initial edgexfoundry test suite
    
    * Revert "Make configure.go works for both device/app and edgex-go services"
    
    This reverts commit 58bfab513e4ad11f0613e6442533fdac8cab6181.
    
    * Add device-virtual's TestEnvChangeServicePort to its subtest
    
    * Move the constants to the scope that they are used
    
    * Group constants for better formatting

---
+++ test/suites/edgexfoundry/config_test.go
@@ -0,0 +1,40 @@
+package test
+
+import (
+	"edgex-snap-testing/test/utils"
+	"testing"
+)
+
+// Deprecated
+func TestEnvConfig(t *testing.T) {
+
+	t.Cleanup(func() {
+		utils.SnapStop(t, snapAppName)
+	})
+	t.Run("change device-virtual service port", func(t *testing.T) {
+		const newPort = "56789"
+		const envServicePort = "env.device-virtual.service.port"
+
+		// make sure the port is available before using it
+		utils.RequirePortAvailable(t, newPort)
+
+		// check if service port can be changed
+		utils.SnapStop(t, snapAppName)
+		utils.SnapSet(t, platformSnap, envServicePort, newPort)
+		utils.SnapStart(t, snapAppName)
+		utils.WaitServiceOnline(t, newPort)
+
+		// check if service port can be unset and revert to the default
+		utils.SnapStop(t, snapAppName)
+		utils.SnapUnset(t, platformSnap, envServicePort)
+		utils.SnapStart(t, snapAppName)
+		utils.WaitServiceOnline(t, deviceVirtualDefaultServicePort)
+
+		utils.SnapStop(t, snapAppName)
+		utils.SnapUnset(t, platformSnap, envServicePort)
+	})
+}
+
+func TestAppConfig(t *testing.T) {
+	t.Skip("TODO")
+}
---
+++ test/suites/edgexfoundry/main_test.go
@@ -0,0 +1,45 @@
+package test
+
+import (
+	"edgex-snap-testing/test/utils"
+	"log"
+	"os"
+	"testing"
+)
+
+const (
+	platformSnap     = "edgexfoundry"
+	deviceVirtualApp = "device-virtual"
+	snapAppName      = platformSnap + "." + deviceVirtualApp
+
+	deviceVirtualDefaultServicePort = "59900"
+)
+
+func TestMain(m *testing.M) {
+
+	log.Println("[SETUP]")
+
+	// start clean
+	utils.SnapRemove(nil,
+		platformSnap,
+	)
+
+	utils.SnapInstallFromStore(nil, platformSnap, utils.PlatformChannel)
+
+	// make sure all services are online before starting the tests
+	utils.WaitServiceOnline(nil, utils.PlatformPorts...)
+	utils.SnapSet(nil, platformSnap, deviceVirtualApp, "on")
+	utils.WaitServiceOnline(nil, deviceVirtualDefaultServicePort)
+
+	exitCode := m.Run()
+
+	log.Println("[TEARDOWN]")
+
+	utils.SnapDumpLogs(nil, platformSnap)
+
+	utils.SnapRemove(nil,
+		platformSnap,
+	)
+
+	os.Exit(exitCode)
+}
