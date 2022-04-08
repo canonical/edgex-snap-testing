757bcec4cace0c78dfe83f23532d3afafbcd64a4
Mengyi Wang <mengyi.wang@canonical.com>
2022-04-08 14:56:36 +0200

    Loop through all platform ports except for kong's and redis's

--- test/suites/edgexfoundry/network_test.go
+++ test/suites/edgexfoundry/network_test.go
@@ -10,6 +10,7 @@ func TestNetworkInterface(t *testing.T) {
 		utils.SnapStop(t, snapAppName)
 	})
 
+	// check network interface status for device-virtual service
 	utils.SnapStart(t, snapAppName)
 
 	t.Run("listen default port "+deviceVirtualDefaultServicePort, func(t *testing.T) {
@@ -24,4 +25,21 @@ func TestNetworkInterface(t *testing.T) {
 		utils.RequireListenLoopback(t, deviceVirtualDefaultServicePort)
 		utils.RequirePortOpen(t, deviceVirtualDefaultServicePort)
 	})
+
+	// check network interface status for all platform ports except for:
+	// Kong’s port: 8000
+	// Kong-db's port: 5432
+	// Redis's port: 6379
+	for _, port := range utils.PlatformPorts {
+		if port != "8000" && port != "5432" && port != "6379" {
+			t.Run("platform port "+port+" not listen on all interfaces", func(t *testing.T) {
+				utils.RequireListenAllInterfaces(t, false, port)
+			})
+
+			t.Run("platform port "+port+" listen localhost", func(t *testing.T) {
+				utils.RequireListenLoopback(t, port)
+				utils.RequirePortOpen(t, port)
+			})
+		}
+	}
 }
