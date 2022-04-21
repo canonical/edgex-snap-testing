8b85086aa5cf7402737142c9c58d413312358b0e
Mengyi Wang <mengyi.wang@canonical.com>
2022-04-21 09:45:42 +0200

    Separate tests

--- test/suites/device-mqtt/config_test.go
+++ test/suites/device-mqtt/config_test.go
@@ -12,9 +12,7 @@ func TestEnvConfig(t *testing.T) {
 
 	t.Run("change service port", func(t *testing.T) {
 		t.Cleanup(func() {
-			utils.SnapUnset(t, deviceMqttSnap, "env.service.port")
-			utils.SnapUnset(t, deviceMqttSnap, "env")
-			utils.SnapRestart(t, deviceMqttSnap)
+			utils.SnapStop(t, deviceMqttSnap)
 		})
 
 		const newPort = "56789"
@@ -22,11 +20,18 @@ func TestEnvConfig(t *testing.T) {
 		// make sure the port is available before using it
 		utils.RequirePortAvailable(t, newPort)
 
+		// set env. and validate the new port comes online
 		utils.SnapStart(t, deviceMqttSnap)
 		utils.SnapSet(t, deviceMqttSnap, "env.service.port", newPort)
-		utils.SnapRestart(t, deviceMqttSnap)
+		utils.SnapRestart(t, deviceMqttService)
 
 		utils.WaitServiceOnline(t, 60, newPort)
+
+		// unset env. and validate the default port comes online
+		utils.SnapUnset(t, deviceMqttSnap, "env.service.port")
+		utils.SnapUnset(t, deviceMqttSnap, "env")
+		utils.SnapRestart(t, deviceMqttService)
+		utils.WaitServiceOnline(t, 60, defaultServicePort)
 	})
 }
 
@@ -34,22 +39,86 @@ func TestAppConfig(t *testing.T) {
 	// start clean
 	utils.SnapStop(t, deviceMqttSnap)
 
-	t.Run("use apps. and config. for the same option", func(t *testing.T) {
+	t.Run("set and unset apps.", func(t *testing.T) {
 		t.Cleanup(func() {
-			utils.SnapUnset(t, deviceMqttSnap, "apps.device-mqtt.config.service.port")
-			utils.SnapUnset(t, deviceMqttSnap, "config.service.port")
-			utils.SnapRestart(t, deviceMqttSnap)
+			utils.SnapStop(t, deviceMqttSnap)
 		})
 
-		const newPort = "8888"
+		const newPort = "1111"
 
 		// make sure the port is available before using it
 		utils.RequirePortAvailable(t, newPort)
 
+		// set apps. and validate the new port comes online
+		utils.SnapStart(t, deviceMqttSnap)
+		utils.SnapSet(t, deviceMqttSnap, "apps.device-mqtt.config.service.port", newPort)
+		utils.SnapRestart(t, deviceMqttService)
+
+		utils.WaitServiceOnline(t, 60, newPort)
+
+		// unset apps. and validate the default port comes online
+		utils.SnapUnset(t, deviceMqttSnap, "apps.device-mqtt.config.service.port")
+		utils.SnapUnset(t, deviceMqttSnap, "config.service.port")
+		utils.SnapRestart(t, deviceMqttService)
+
+		utils.WaitServiceOnline(t, 60, defaultServicePort)
+	})
+}
+
+func TestGlobalConfig(t *testing.T) {
+	// start clean
+	utils.SnapStop(t, deviceMqttSnap)
+
+	t.Run("set and unset apps.", func(t *testing.T) {
+		t.Cleanup(func() {
+			utils.SnapStop(t, deviceMqttSnap)
+		})
+
+		const newPort = "2222"
+
+		// make sure the port is available before using it
+		utils.RequirePortAvailable(t, newPort)
+
+		// set config. and validate the new port comes online
+		utils.SnapStart(t, deviceMqttSnap)
+		utils.SnapSet(t, deviceMqttSnap, "config.service.port", newPort)
+		utils.SnapRestart(t, deviceMqttService)
+
+		utils.WaitServiceOnline(t, 60, newPort)
+
+		// unset config. and validate the default port comes online
+		utils.SnapUnset(t, deviceMqttSnap, "config.service.port")
+		utils.SnapRestart(t, deviceMqttService)
+
+		utils.WaitServiceOnline(t, 60, defaultServicePort)
+	})
+}
+
+func TestAppGlobalConfig(t *testing.T) {
+	// start clean
+	utils.SnapStop(t, deviceMqttSnap)
+
+	t.Run("use apps. and config. for the same option", func(t *testing.T) {
+		t.Cleanup(func() {
+			utils.SnapUnset(t, deviceMqttSnap, "apps.device-mqtt.config.service.port")
+			utils.SnapUnset(t, deviceMqttSnap, "config.service.port")
+			utils.SnapRestart(t, deviceMqttService)
+			utils.WaitServiceOnline(t, 60, defaultServicePort)
+
+			utils.SnapStop(t, deviceMqttSnap)
+		})
+
+		const newPort = "33333"
+
+		// make sure the port is available before using it
+		utils.RequirePortAvailable(t, newPort)
+
+		// set apps. and config. with the same option,
+		// and validate that option has been picked upp by the service
 		utils.SnapStart(t, deviceMqttSnap)
 		utils.SnapSet(t, deviceMqttSnap, "apps.device-mqtt.config.service.port", newPort)
 		utils.SnapSet(t, deviceMqttSnap, "config.service.port", newPort)
-		utils.SnapRestart(t, deviceMqttSnap)
+		utils.SnapRestart(t, deviceMqttService)
 
 		utils.WaitServiceOnline(t, 60, newPort)
 	})
@@ -58,20 +127,22 @@ func TestAppConfig(t *testing.T) {
 		t.Cleanup(func() {
 			utils.SnapUnset(t, deviceMqttSnap, "apps.device-mqtt.config.service.port")
 			utils.SnapUnset(t, deviceMqttSnap, "config.service.port")
-			utils.SnapRestart(t, deviceMqttSnap)
+			utils.SnapRestart(t, deviceMqttService)
 		})
 
-		const newAppPort = "11111"
-		const newConfigPort = "22222"
+		const newAppPort = "44444"
+		const newConfigPort = "55555"
 
 		// make sure the ports are available before using it
 		utils.RequirePortAvailable(t, newAppPort)
 		utils.RequirePortAvailable(t, newConfigPort)
 
+		// set apps. and config. with different options,
+		// and validate that app-specific option has been picked up with higher precedence
 		utils.SnapStart(t, deviceMqttSnap)
 		utils.SnapSet(t, deviceMqttSnap, "apps.device-mqtt.config.service.port", newAppPort)
 		utils.SnapSet(t, deviceMqttSnap, "config.service.port", newConfigPort)
-		utils.SnapRestart(t, deviceMqttSnap)
+		utils.SnapRestart(t, deviceMqttService)
 
 		utils.WaitServiceOnline(t, 60, newAppPort)
 	})
