6814086c5f455060d78ea17fd5243494c68a6a83
Farshid Tavakolizadeh <farshid.tavakolizadeh@canonical.com>
2022-04-26 10:16:44 +0200

    Add config testing for device-snmp

--- test/suites/device-snmp/config_test.go
+++ test/suites/device-snmp/config_test.go
@@ -7,11 +7,18 @@ import (
 
 // Deprecated
 func TestEnvConfig(t *testing.T) {
-	t.Run("change service port", func(t *testing.T) {
-		utils.TestEnvChangeServicePort(t, deviceSnmpSnap, defaultServicePort)
-	})
+	utils.SetEnvConfig(t, deviceSnmpSnap, deviceSnmpService, defaultServicePort)
 }
 
 func TestAppConfig(t *testing.T) {
-	t.Skip("TODO")
+	utils.SetAppConfig(t, deviceSnmpSnap, deviceSnmpService, deviceSnmpApp, defaultServicePort)
+}
+
+func TestGlobalConfig(t *testing.T) {
+	// start clean
+	utils.SetGlobalConfig(t, deviceSnmpSnap, deviceSnmpService, defaultServicePort)
+}
+
+func TestMixedConfig(t *testing.T) {
+	utils.SetMixedConfig(t, deviceSnmpSnap, deviceSnmpService, deviceSnmpApp, defaultServicePort)
 }
