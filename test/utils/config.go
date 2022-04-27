90531fe4c0881449eb92dd693eb54174d2f0294d
Mengyi Wang <mengyi.wang@canonical.com>
2022-04-27 11:51:11 +0200

    remove not needed `else` after `t.SKip()`

--- test/utils/config.go
+++ test/utils/config.go
@@ -5,36 +5,36 @@ import "testing"
 func SetEnvConfig(t *testing.T, snap, app, servicePort string) {
 	service := snap + "." + app
 	if !FullConfigTest {
-		// make this subtest optional to save testing time,
-		t.Skip("Full config test is disabled by default, and similar full config tests have been operated in device-mqtt test suite.")
-	} else {
-		// start clean
-		SnapStop(t, service)
-
-		t.Run("change service port", func(t *testing.T) {
-			t.Cleanup(func() {
-				SnapUnset(t, snap, "env")
-				SnapStop(t, service)
-			})
-
-			const newPort = "11111"
-
-			// make sure the port is available before using it
-			RequirePortAvailable(t, newPort)
-
-			// set env. and validate the new port comes online
-			SnapSet(t, snap, "env.service.port", newPort)
-			SnapStart(t, service)
-
-			WaitServiceOnline(t, 60, newPort)
-
-			// unset env. and validate the default port comes online
-			SnapUnset(t, snap, "env.service.port")
-			SnapRestart(t, service)
-			WaitServiceOnline(t, 60, defaultServicePort)
-		})
+		t.Skip("Full config test is disabled.")
 	}
+	// start clean
+	SnapStop(t, service)
+
+	t.Run("change service port", func(t *testing.T) {
+		t.Cleanup(func() {
+			SnapUnset(t, snap, "env")
+			SnapStop(t, service)
+		})
+
+		const newPort = "11111"
+
+		// make sure the port is available before using it
+		RequirePortAvailable(t, newPort)
+
+		// set env. and validate the new port comes online
+		SnapSet(t, snap, "env.service.port", newPort)
+		SnapStart(t, service)
+
+		WaitServiceOnline(t, 60, newPort)
+
+		// unset env. and validate the default port comes online
+		SnapUnset(t, snap, "env.service.port")
+		SnapRestart(t, service)
+		WaitServiceOnline(t, 60, servicePort)
+	})
+
 }
+
 func SetAppConfig(t *testing.T, snap, app, servicePort string) {
 	service := snap + "." + app
 
@@ -109,38 +109,36 @@ func SetMixedConfig(t *testing.T, snap, app, servicePort string) {
 	service := snap + "." + app
 
 	if !FullConfigTest {
-		// make this subtest optional to save testing time,
-		// similar full config tests have been operated in device-mqtt test suite
-		t.Skip("Full config test is disabled by default, and similar full config tests have been operated in device-mqtt test suite.")
-	} else {
-		// start clean
-		SnapStop(t, service)
-
-		t.Run("use apps. and config. for different values", func(t *testing.T) {
-			t.Cleanup(func() {
-				SnapUnset(t, snap, "apps")
-				SnapUnset(t, snap, "config")
-				SnapUnset(t, snap, "config-enabled")
-				SnapStop(t, service)
-			})
-
-			const newAppPort = "44444"
-			const newConfigPort = "55555"
-
-			// enable new apps/config options to aviod mixed options issue with old env option
-			SnapSet(t, snap, "config-enabled", "true")
-
-			// make sure the ports are available before using it
-			RequirePortAvailable(t, newAppPort)
-			RequirePortAvailable(t, newConfigPort)
-
-			// set apps. and config. with different values,
-			// and validate that app-specific option has been picked up because it has higher precedence
-		SnapSet(t, snap, "apps."+app+".config.service-port", newAppPort)
-			SnapSet(t, snap, "config.service-port", newConfigPort)
-			SnapStart(t, service)
-
-			WaitServiceOnline(t, 60, newAppPort)
-		})
+		t.Skip("Full config test is disabled.")
 	}
+	// start clean
+	SnapStop(t, service)
+
+	t.Run("use apps. and config. for different values", func(t *testing.T) {
+		t.Cleanup(func() {
+			SnapUnset(t, snap, "apps")
+			SnapUnset(t, snap, "config")
+			SnapUnset(t, snap, "config-enabled")
+			SnapStop(t, service)
+		})
+
+		const newAppPort = "44444"
+		const newConfigPort = "55555"
+
+		// enable new apps/config options to aviod mixed options issue with old env option
+		SnapSet(t, snap, "config-enabled", "true")
+
+		// make sure the ports are available before using it
+		RequirePortAvailable(t, newAppPort)
+		RequirePortAvailable(t, newConfigPort)
+
+		// set apps. and config. with different values,
+		// and validate that app-specific option has been picked up because it has higher precedence
+		SnapSet(t, snap, "apps."+app+".config.service-port", newAppPort)
+		SnapSet(t, snap, "config.service-port", newConfigPort)
+		SnapStart(t, service)
+
+		WaitServiceOnline(t, 60, newAppPort)
+	})
+
 }
