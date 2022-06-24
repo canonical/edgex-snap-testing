a0838c35644b1d0fc4f0276d16679648bfe23300
Mengyi Wang <mengyi.wang@canonical.com>
2022-06-24 10:58:09 +0200

    Split one proxy test to two tests

--- test/suites/edgexfoundry/proxy_test.go
+++ test/suites/edgexfoundry/proxy_test.go
@@ -19,6 +19,7 @@ const (
 	KONG_ADMIN_JWT_FILE    = EDGEXFOUNDRY_SNAP_DATA + "/secrets/security-proxy-setup/kong-admin-jwt"
 )
 
+func TestAddProxyUser(t *testing.T) {
 
 	// start clean
 	utils.Exec(t, `rm -rf `+CA_KEY_FILE+` `+CA_CERT_FILE+` `+SERVER_KEY_FILE+` `+SERVER_CSR_FILE+` `+SERVER_CERT_FILE+` `+EDGEXFOUNDRY_SNAP_DATA)
@@ -27,47 +28,60 @@ const (
 		utils.Exec(t, `rm -rf `+CA_KEY_FILE+` `+CA_CERT_FILE+` `+SERVER_KEY_FILE+` `+SERVER_CSR_FILE+` `+SERVER_CERT_FILE+` `+EDGEXFOUNDRY_SNAP_DATA)
 	})
 
-	// Due to confinement issues when running this test, we write the private key to SNAP_DATA
-	// Create private and public keys
+	// Due to confinement issues when running this test, we write the files to SNAP_DATA
 	utils.Exec(t, `sudo mkdir -p `+EDGEXFOUNDRY_SNAP_DATA)
-	utils.Exec(t, `sudo openssl ecparam -genkey -name prime256v1 -noout -out `+PRIVATE_KEY)
-	utils.Exec(t, `sudo openssl ec -in `+PRIVATE_KEY+` -pubout -out `+PUBLIC_KEY)
 
 	// Read the API Gateway token
 	KONG_ADMIN_JWT, _ := utils.Exec(t, "sudo cat /var/snap/edgexfoundry/current/secrets/security-proxy-setup/kong-admin-jwt")
 	require.NotEmpty(t, KONG_ADMIN_JWT)
 
-	t.Run("add proxy admin user", func(t *testing.T) {
-		// Use secrets-config to add a user example with id 1000
-		// On success, the above command prints the user id
-		stdout, _ := utils.Exec(t, `
+	// Create private and public keys
+	utils.Exec(t, `sudo openssl ecparam -genkey -name prime256v1 -noout -out `+PRIVATE_KEY)
+	utils.Exec(t, `sudo openssl ec -in `+PRIVATE_KEY+` -pubout -out `+PUBLIC_KEY)
+
+	// Use secrets-config to add a user example with id 1000
+	// On success, the above command prints the user id
+	stdout, _ := utils.Exec(t, `
 			sudo edgexfoundry.secrets-config proxy adduser \
 			--token-type jwt --user example --algorithm ES256 \
 			--public_key `+PUBLIC_KEY+` --id 1000 -jwt `+KONG_ADMIN_JWT)
-		require.Equal(t, "1000\n", stdout)
+	require.Equal(t, "1000\n", stdout)
+
+}
+
+func TestTLSCert(t *testing.T) {
+
+	// start clean
+	utils.Exec(t, `rm -rf `+CA_KEY_FILE+` `+CA_CERT_FILE+` `+SERVER_KEY_FILE+` `+SERVER_CSR_FILE+` `+SERVER_CERT_FILE+` `+EDGEXFOUNDRY_SNAP_DATA)
+
+	t.Cleanup(func() {
+		utils.Exec(t, `rm -rf `+CA_KEY_FILE+` `+CA_CERT_FILE+` `+SERVER_KEY_FILE+` `+SERVER_CSR_FILE+` `+SERVER_CERT_FILE+` `+EDGEXFOUNDRY_SNAP_DATA)
 	})
 
-	t.Run("set custom TLS cert", func(t *testing.T) {
-		certGenerator(CA_KEY_FILE, CA_CERT_FILE, SERVER_KEY_FILE, SERVER_CSR_FILE, SERVER_CERT_FILE)
+	// Due to confinement issues when running this test, we write the files to SNAP_DATA
+	utils.Exec(t, `sudo mkdir -p `+EDGEXFOUNDRY_SNAP_DATA)
 
-		// Setting security-proxy certificate
-		utils.SnapSet(t, "edgexfoundry", "env.security-proxy.tls-certificate", SERVER_CERT_FILE)
+	// Read the API Gateway token
+	KONG_ADMIN_JWT, _ := utils.Exec(t, "sudo cat /var/snap/edgexfoundry/current/secrets/security-proxy-setup/kong-admin-jwt")
+	require.NotEmpty(t, KONG_ADMIN_JWT)
 
-		// Setting security-proxy certificate private key
-		utils.SnapSet(t, "edgexfoundry", "env.security-proxy.tls-private-key", SERVER_KEY_FILE)
+	certGenerator(CA_KEY_FILE, CA_CERT_FILE, SERVER_KEY_FILE, SERVER_CSR_FILE, SERVER_CERT_FILE)
+	SERVER_CERT, _ := utils.Exec(t, `sudo cat `+SERVER_CERT_FILE)
+	SERVER_KEY, _ := utils.Exec(t, `sudo cat `+SERVER_KEY_FILE)
 
-		utils.Exec(t, `cp `+EDGEXFOUNDRY_SNAP_DATA+`/ca.crt `+EDGEXFOUNDRY_SNAP_DATA)
+	// Setting security-proxy certificate
+	utils.SnapSet(t, "edgexfoundry", "env.security-proxy.tls-certificate", `"`+SERVER_CERT+`"`)
+	// Setting security-proxy certificate private key
+	utils.SnapSet(t, "edgexfoundry", "env.security-proxy.tls-private-key", `"`+SERVER_KEY+`"`)
+	// Add the certificate, using Kong Admin JWT to authenticate
+	utils.Exec(t, `sudo edgexfoundry.secrets-config proxy tls --incert `+SERVER_CERT_FILE+` --inkey `+SERVER_KEY_FILE+` --admin_api_jwt `+KONG_ADMIN_JWT_FILE)
 
-		// Add the certificate, using Kong Admin JWT to authenticate
-		utils.Exec(t, `sudo edgexfoundry.secrets-config proxy tls --incert cert.pem --inkey `+PRIVATE_KEY+` --admin_api_jwt `+KONG_ADMIN_JWT)
-		code, _ := utils.Exec(t, `curl --show-error --silent --include \
+	code, _ := utils.Exec(t, `curl --show-error --silent --include \
 		--output /dev/null --write-out "%{http_code}" \
-		--cacert `+CA_KEY_FILE+` \
+		--cacert `+CA_CERT_FILE+` \
 		-X GET 'https://localhost:8443/core-data/api/v2/ping?' \
 		-H "Authorization: Bearer $TOKEN"`)
-		require.Equal(t, 200, code)
-
-	})
+	require.Equal(t, "200\n", code)
 }
 
 func certGenerator(CA_KEY_FILE, CA_CERT_FILE, SERVER_KEY_FILE, SERVER_CSR_FILE, SERVER_CERT_FILE string) {
