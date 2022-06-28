b06e081df06475f34721ceef81a284a215a719c4
Mengyi Wang <mengyi.wang@canonical.com>
2022-06-28 08:58:47 +0200

    Update TestAddProxyUser and TestTLSCert

--- test/suites/edgexfoundry/proxy_test.go
+++ test/suites/edgexfoundry/proxy_test.go
@@ -1,100 +1,128 @@
 package test
 
 import (
+	"crypto/tls"
 	"edgex-snap-testing/test/utils"
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
-const (
-	snapData        = "/var/snap/edgexfoundry/current/temp"
-	kongAminJwtFile = "/var/snap/edgexfoundry/current/secrets/security-proxy-setup/kong-admin-jwt"
-)
+var kongAdminJwtFile = "/var/snap/edgexfoundry/current/secrets/security-proxy-setup/kong-admin-jwt"
 
 func TestAddProxyUser(t *testing.T) {
-	const (
-		publicKey  = snapData + "/public.pem"
-		privateKey = snapData + "/private.pem"
-	)
+
+	var tmpDir = "keys"
 
 	// start clean
-	utils.Exec(t, `sudo rm -rf `+snapData)
+	err := os.RemoveAll(tmpDir)
+	require.NoError(t, err)
 
 	t.Cleanup(func() {
-		utils.Exec(t, `sudo rm -rf `+snapData)
+		err = os.RemoveAll(tmpDir)
+		require.NoError(t, err)
 	})
 
-	// Due to confinement issues when running this test, we write the files to SNAP_DATA
-	utils.Exec(t, `sudo mkdir -p `+snapData)
-
-	// Read the API Gateway token
-	kongAdminJWT, _ := utils.Exec(t, "sudo cat "+kongAminJwtFile)
+	// Get Kong admin JWT token
+	out, err := os.ReadFile(kongAdminJwtFile)
+	require.NoError(t, err)
+	kongAdminJWT := string(out)
 	require.NotEmpty(t, kongAdminJWT)
 
-	// Create private and public keys
-	utils.Exec(t, `sudo openssl ecparam -genkey -name prime256v1 -noout -out `+privateKey)
-	utils.Exec(t, `sudo openssl ec -in `+privateKey+` -pubout -out `+publicKey)
+	// Create temp dir for private and public keys
+	err = os.Mkdir(tmpDir, 0755)
+	require.NoError(t, err)
+
+	publicKey := tmpDir + "/public.pem"
+	privateKey := tmpDir + "/private.pem"
+
+	// Generate private and public keys
+	utils.Exec(t, fmt.Sprintf("openssl ecparam -genkey -name prime256v1 -noout -out %s", privateKey))
+	utils.Exec(t, fmt.Sprintf("openssl ec -in %s -pubout -out %s", privateKey, publicKey))
 
 	// Use secrets-config to add a user example with id 1000
+	stdout, _ := utils.Exec(t,
+		fmt.Sprintf("edgexfoundry.secrets-config proxy adduser --token-type jwt --user example --algorithm ES256 --public_key %s --id 1000 -jwt %s",
+			publicKey,
+			kongAdminJWT))
 	// On success, the above command prints the user id
-	stdout, _ := utils.Exec(t, `
-			sudo edgexfoundry.secrets-config proxy adduser \
-			--token-type jwt --user example --algorithm ES256 \
-			--public_key `+publicKey+` --id 1000 -jwt `+kongAdminJWT)
 	require.Equal(t, "1000\n", stdout)
 
+	// Generate a JWT token for the above example user
+	jwt, _ := utils.Exec(t,
+		fmt.Sprintf("edgexfoundry.secrets-config proxy jwt --algorithm ES256 --private_key %s --id 1000 --expiration=1h", privateKey))
+	jwt = strings.TrimSuffix(jwt, "\n")
+
+	// Call an API on behalf of example user
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	client := &http.Client{Transport: tr}
+
+	req, err := http.NewRequest("GET", "https://localhost:8443/core-data/api/v2/ping", nil)
+	require.NoError(t, err)
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", jwt))
+
+	resp, err := client.Do(req)
+	require.NoError(t, err)
+	defer resp.Body.Close()
 }
 
 func TestTLSCert(t *testing.T) {
+
+	var tmpDir = "certs"
+
 	// start clean
-	utils.Exec(t, `sudo rm -rf `+snapData)
+	err := os.RemoveAll(tmpDir)
+	require.NoError(t, err)
 
 	t.Cleanup(func() {
-		utils.Exec(t, `sudo rm -rf `+snapData)
+		err = os.RemoveAll(tmpDir)
+		require.NoError(t, err)
 	})
 
-	// Due to confinement issues when running this test, we write the files to SNAP_DATA
-	utils.Exec(t, `sudo mkdir -p `+snapData)
+	// Get Kong admin JWT token
+	out, err := os.ReadFile(kongAdminJwtFile)
+	require.NoError(t, err)
+	kongAdminJWT := string(out)
+	require.NotEmpty(t, kongAdminJWT)
 
-	// Read the API Gateway token
-	kongAminJwt, _ := utils.Exec(t, "sudo cat "+kongAminJwtFile)
-	require.NotEmpty(t, kongAminJwt)
+	// Create temp dir for certificates and keys
+	err = os.Mkdir(tmpDir, 0755)
+	require.NoError(t, err)
 
 	// Add the certificate, using Kong Admin JWT to authenticate
-	caCertFile, caKeyFile := certGenerator()
-	utils.Exec(t, `sudo edgexfoundry.secrets-config proxy tls --incert `+caCertFile+` --inkey `+caKeyFile+` --admin_api_jwt `+kongAminJwt)
+	caCertFile, caKeyFile := certGenerator(tmpDir)
+	utils.Exec(t, fmt.Sprintf("edgexfoundry.secrets-config proxy tls --incert %s --inkey %s --admin_api_jwt %s", caCertFile, caKeyFile, kongAdminJWT))
 
-	code, _ := utils.Exec(t, `curl --show-error --silent --include \
-		--output /dev/null --write-out "%{http_code}" \
-		--cacert `+caCertFile+` \
-		-X GET 'https://localhost:8443/core-data/api/v2/ping?' \
-		-H "Authorization: Bearer "`+kongAminJwt)
-	require.Equal(t, "200\n", code)
-
+	code, _ := utils.Exec(t, fmt.Sprintf(`curl --show-error --silent --include --output /dev/null --write-out "%%{http_code}" --cacert %s -X GET 'https://localhost:8443/core-data/api/v2/ping?' -H "Authorization: Bearer %s"`, caCertFile, "BadToken"))
+	require.Equal(t, "401\n", code)
 }
 
-func certGenerator() (string, string) {
-	const (
-		caKeyFile  = snapData + "/ca.key"
-		caCertFile = snapData + "/ca.crt"
-
-		serverCsrFile  = snapData + "/server.csr"
-		serverKeyFile  = snapData + "/server.key"
-		serverCertFile = snapData + "/server.crt"
+func certGenerator(tmpDir string) (string, string) {
+	var (
+		caKeyFile      = tmpDir + "/ca.key"
+		caCertFile     = tmpDir + "/ca.crt"
+		serverCsrFile  = tmpDir + "/server.csr"
+		serverKeyFile  = tmpDir + "/server.key"
+		serverCertFile = tmpDir + "/server.crt"
 	)
 
 	// Generate the Certificate Authority (CA) Private Key
-	utils.Exec(nil, `sudo openssl ecparam -name prime256v1 -genkey -noout -out `+caKeyFile)
+	utils.Exec(nil, fmt.Sprintf("openssl ecparam -name prime256v1 -genkey -noout -out %s", caKeyFile))
 	// Generate the Certificate Authority Certificate
-	utils.Exec(nil, `sudo openssl req -new -x509 -sha256 -key `+caKeyFile+` -out `+caCertFile+` -subj "/CN=localhost"`)
+	utils.Exec(nil, fmt.Sprintf(`openssl req -new -x509 -sha256 -key %s -out %s -subj "/CN=localhost"`, caKeyFile, caCertFile))
 
-	// Generate the Server Certificate Private Key
-	utils.Exec(nil, `sudo openssl ecparam -name prime256v1 -genkey -noout -out `+serverKeyFile)
+	// Generate the Server Certificate Private Keys
+	utils.Exec(nil, fmt.Sprintf("openssl ecparam -name prime256v1 -genkey -noout -out %s", serverKeyFile))
 	// Generate the Server Certificate Signing Request
-	utils.Exec(nil, `sudo openssl req -new -sha256 -key `+serverKeyFile+` -out `+serverCsrFile+` -subj "/CN=localhost"`)
+	utils.Exec(nil, fmt.Sprintf(`openssl req -new -sha256 -key %s -out %s -subj "/CN=localhost"`, serverKeyFile, serverCsrFile))
 	// Generate the Server Certificate
-	utils.Exec(nil, `sudo openssl x509 -req -in `+serverCsrFile+` -CA `+caCertFile+` -CAkey `+caKeyFile+` -CAcreateserial -out `+serverCertFile+` -days 1000 -sha256`)
+	utils.Exec(nil, fmt.Sprintf(`openssl x509 -req -in %s -CA %s -CAkey %s -CAcreateserial -out %s -days 1000 -sha256`, serverCsrFile, caCertFile, caKeyFile, serverCertFile))
 
 	return caCertFile, caKeyFile
 }
