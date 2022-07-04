e6c8bff26617a623ec19a04ef8244e0e0332a02d
Farshid Tavakolizadeh <farshid.tavakolizadeh@canonical.com>
2022-07-04 11:42:59 +0200

    Refactor proxy tests (#77)
    * Refactor proxy tests
    
    * Add delay to allow certificate use by the proxy

--- test/suites/edgexfoundry/proxy_test.go
+++ test/suites/edgexfoundry/proxy_test.go
@@ -44,10 +44,9 @@ func TestAddProxyUser(t *testing.T) {
 	// Use secrets-config to add a user example with id 1000
 	stdout, _ := utils.Exec(t,
 		fmt.Sprintf("edgexfoundry.secrets-config proxy adduser --token-type jwt --user example --algorithm ES256 --public_key %s --id 1000 -jwt %s",
-			publicKey,
-			kongAdminJWT))
+			publicKey, kongAdminJWT))
 	// On success, the above command prints the user id
-	require.Equal(t, "1000\n", stdout)
+	require.Equal(t, "1000", strings.TrimSpace(stdout))
 
 	// Generate a JWT token for the above example user
 	jwt, _ := utils.Exec(t,
@@ -85,83 +84,69 @@ func TestTLSCert(t *testing.T) {
 	// Create temp dir for certificates and keys
 	require.NoError(t, os.Mkdir(tmpDir, 0755))
 
-	// Get Kong admin JWT token
+	t.Logf("Get Kong admin JWT token")
 	utils.Exec(t, fmt.Sprintf("sudo install -m 604 /var/snap/edgexfoundry/current/secrets/security-proxy-setup/kong-admin-jwt ./%s", tmpDir))
 	kongAdminJWTFile := tmpDir + "/kong-admin-jwt"
 	kongAdminJWT, err := os.ReadFile(kongAdminJWTFile)
 	require.NoError(t, err)
 
-	// Add the certificate, using Kong Admin JWT to authenticate
-	caCertFile, caKeyFile := generateCerts(tmpDir)
-	utils.Exec(t, fmt.Sprintf("edgexfoundry.secrets-config proxy tls --incert %s --inkey %s --admin_api_jwt %s", caCertFile, caKeyFile, kongAdminJWT))
+	t.Logf("Generate a self-signed certificate")
+	caKeyFile, caCertFile, _, _, _ := generateCerts(tmpDir)
 
-	// Wait the certificate to be fully installed
+	t.Logf("Add the self-signed certificate")
+	utils.Exec(t, fmt.Sprintf("edgexfoundry.secrets-config proxy tls --incert %s --inkey %s --admin_api_jwt %s",
+		caCertFile, caKeyFile, kongAdminJWT))
+
+	t.Logf("Verify certificate installation by querying Kong's Admin API")
+	// Query installed certificates from Kong
+	resp, err := http.Get("http://localhost:8001/certificates")
+	require.NoError(t, err)
+	defer resp.Body.Close()
+	var res struct{ Data []struct{ Key string } }
+	err = json.NewDecoder(resp.Body).Decode(&res)
+	require.NoError(t, err)
+	require.Len(t, res.Data, 1)
+
 	caKey, err := os.ReadFile(caKeyFile)
 	require.NoError(t, err)
-	waitCertInstall(t, string(caKey), 10)
+	require.Equal(t, res.Data[0].Key, string(caKey))
 
-	// Check if TLS is setup correctly returning status code 401
-	code, _ := utils.Exec(t, fmt.Sprintf(`curl --show-error --silent --include --output /dev/null --write-out "%%{http_code}" --cacert %s -X GET 'https://localhost:8443/core-data/api/v2/ping' -H "Authorization: Bearer %s"`, caCertFile, "testToken"))
-	require.Equal(t, "401\n", code)
+	// Note: Certificate installation doesn't imply that the server immediately starts using it for serving requests
+	time.Sleep(3 * time.Second)
+
+	t.Logf("Query a service via the proxy to verify the use of new certificate")
+	// A success response should return status 401 because the endpoint is protected.
+	// Note: %%	is a literal percent sign
+	code, _ := utils.Exec(t, fmt.Sprintf("curl --show-error --silent --include --output /dev/null --write-out '%%{http_code}' --cacert %s -X GET 'https://localhost:8443/core-data/api/v2/ping'",
+		caCertFile))
+	require.Equal(t, "401", strings.TrimSpace(code))
 }
 
 // generateCerts generates CA private key, CA cert,
 // server private key, server signing request, server cert
-func generateCerts(tmpDir string) (string, string) {
-	var (
-		caKeyFile      = tmpDir + "/ca.key"
-		caCertFile     = tmpDir + "/ca.crt"
-		serverCsrFile  = tmpDir + "/server.csr"
-		serverKeyFile  = tmpDir + "/server.key"
-		serverCertFile = tmpDir + "/server.crt"
-	)
+func generateCerts(dir string) (caKeyFile, caCertFile, serverCsrFile, serverKeyFile, serverCertFile string) {
+	caKeyFile = dir + "/ca.key"
+	caCertFile = dir + "/ca.crt"
+	serverCsrFile = dir + "/server.csr"
+	serverKeyFile = dir + "/server.key"
+	serverCertFile = dir + "/server.crt"
 
 	// Generate the Certificate Authority (CA) Private Key
-	utils.Exec(nil, fmt.Sprintf("openssl ecparam -name prime256v1 -genkey -noout -out %s", caKeyFile))
+	utils.Exec(nil, fmt.Sprintf("openssl ecparam -name prime256v1 -genkey -noout -out %s",
+		caKeyFile))
 	// Generate the Certificate Authority Certificate
-	utils.Exec(nil, fmt.Sprintf(`openssl req -new -x509 -sha256 -key %s -out %s -subj "/CN=localhost"`, caKeyFile, caCertFile))
+	utils.Exec(nil, fmt.Sprintf("openssl req -new -x509 -sha256 -key %s -out %s -subj '/CN=localhost'",
+		caKeyFile, caCertFile))
 
 	// Generate the Server Certificate Private Keys
-	utils.Exec(nil, fmt.Sprintf("openssl ecparam -name prime256v1 -genkey -noout -out %s", serverKeyFile))
+	utils.Exec(nil, fmt.Sprintf("openssl ecparam -name prime256v1 -genkey -noout -out %s",
+		serverKeyFile))
 	// Generate the Server Certificate Signing Request
-	utils.Exec(nil, fmt.Sprintf(`openssl req -new -sha256 -key %s -out %s -subj "/CN=localhost"`, serverKeyFile, serverCsrFile))
+	utils.Exec(nil, fmt.Sprintf("openssl req -new -sha256 -key %s -out %s -subj '/CN=localhost'",
+		serverKeyFile, serverCsrFile))
 	// Generate the Server Certificate
-	utils.Exec(nil, fmt.Sprintf(`openssl x509 -req -in %s -CA %s -CAkey %s -CAcreateserial -out %s -days 1000 -sha256`, serverCsrFile, caCertFile, caKeyFile, serverCertFile))
+	utils.Exec(nil, fmt.Sprintf("openssl x509 -req -in %s -CA %s -CAkey %s -CAcreateserial -out %s -days 1000 -sha256",
+		serverCsrFile, caCertFile, caKeyFile, serverCertFile))
 
-	return caCertFile, caKeyFile
-}
-
-// waitCertInstall checks if certificate is fully installed
-// up to a maximum retry number
-func waitCertInstall(t *testing.T, caKey string, maxRetry int) {
-
-	type ResponseBody struct {
-		Data []struct {
-			Key string
-		}
-	}
-
-	var res ResponseBody
-
-	for i := 1; ; i++ {
-		t.Logf("Checking certificate installation. Retry %d/%d", i, maxRetry)
-
-		resp, err := http.Get("http://localhost:8001/certificates")
-		require.NoError(t, err)
-		defer resp.Body.Close()
-
-		err = json.NewDecoder(resp.Body).Decode(&res)
-		require.NoError(t, err)
-
-		totalCerts := len(res.Data)
-		if i == maxRetry && totalCerts == 0 {
-			t.Fatalf("Time out: reached max %d retries.", maxRetry)
-		} else if totalCerts == 0 {
-			time.Sleep(1 * time.Second)
-		} else {
-			break
-		}
-	}
-
-	require.Equal(t, res.Data[0].Key, caKey)
+	return
 }
