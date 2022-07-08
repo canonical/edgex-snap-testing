eed6b091886def269f6eb50b7e9934ce217c902c
Mengyi Wang <mengyi.wang@canonical.com>
2022-07-08 09:12:24 +0200

    Add device ONVIF camera test suite

---
+++ test/suites/device-onvif-camera/main_test.go
@@ -0,0 +1,114 @@
+package test
+
+import (
+	"edgex-snap-testing/test/utils"
+	"fmt"
+	"log"
+	"os"
+	"testing"
+	"time"
+)
+
+const (
+	deviceOnvifcameraSnap        = "edgex-device-onvif-camera"
+	deviceOnvifCameraApp         = "device-onvif-camera"
+	deviceOnvifcameraService     = deviceOnvifcameraSnap + "." + deviceOnvifCameraApp
+	deviceOnvifcameraServicePort = "59984"
+)
+
+var start = time.Now()
+
+func TestMain(m *testing.M) {
+
+	log.Println("[SETUP]")
+
+	// start clean
+	utils.SnapRemove(nil,
+		deviceOnvifcameraSnap,
+		"edgexfoundry",
+	)
+
+	// install the device snap before edgexfoundry
+	// to catch build error sooner and stop
+	if utils.LocalSnap != "" {
+		utils.SnapInstallFromFile(nil, utils.LocalSnap)
+	} else {
+		utils.SnapInstallFromStore(nil, deviceOnvifcameraSnap, utils.ServiceChannel)
+	}
+	utils.SnapInstallFromStore(nil, "edgexfoundry", utils.PlatformChannel)
+
+	// make sure all services are online before starting the tests
+	utils.WaitPlatformOnline(nil)
+
+	// for local build, the interface isn't auto-connected.
+	// connect manually regardless
+	utils.SnapConnect(nil,
+		"edgexfoundry:edgex-secretstore-token",
+		deviceOnvifcameraSnap+":edgex-secretstore-token",
+	)
+
+	// seed test onvif credentials
+	testData := []byte(`{
+		"secrets": [
+			{
+				"path": "credentials001",
+				"imported": false,
+				"secretData": [
+					{
+						"key": "username",
+						"value": "admin"
+					},
+									{
+						"key": "password",
+						"value": "admin"
+					},
+					{
+						"key": "mode",
+						"value": "usernametoken"
+					}
+				]
+			}
+		]
+	}`)
+	err := os.WriteFile("/var/snap/edgex-device-onvif-camera/x1/device-onvif-camera/onvif-credentials.json", testData, 0644)
+	if err != nil {
+		fmt.Print(err)
+		return
+	}
+
+	exitCode := m.Run()
+
+	log.Println("[TEARDOWN]")
+
+	utils.SnapDumpLogs(nil, start, deviceOnvifcameraSnap)
+
+	utils.SnapRemove(nil,
+		deviceOnvifcameraSnap,
+		"edgexfoundry",
+	)
+
+	os.Exit(exitCode)
+}
+
+func TestCommon(t *testing.T) {
+	utils.TestConfig(t, deviceOnvifcameraSnap, utils.Config{
+		TestChangePort: utils.ConfigChangePort{
+			App:                      deviceOnvifCameraApp,
+			DefaultPort:              deviceOnvifcameraServicePort,
+			TestLegacyEnvConfig:      utils.FullConfigTest,
+			TestAppConfig:            true,
+			TestGlobalConfig:         true,
+			TestMixedGlobalAppConfig: utils.FullConfigTest,
+		},
+	})
+
+	utils.TestNet(t, deviceOnvifcameraSnap, utils.Net{
+		StartSnap:        true,
+		TestOpenPorts:    []string{deviceOnvifcameraServicePort},
+		TestBindLoopback: []string{deviceOnvifcameraServicePort},
+	})
+
+	utils.TestPackaging(t, deviceOnvifcameraSnap, utils.Packaging{
+		TestSemanticSnapVersion: true,
+	})
+}
