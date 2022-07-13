fdf20afebd974ca9ac30e0d6cc0cbf3934827bee
Mengyi Wang <mengyi.wang@canonical.com>
2022-07-13 16:48:35 +0200

    Use $SNAP_DATA/config/device-onvif-camera for ONVIF credentials

--- test/suites/device-onvif-camera/main_test.go
+++ test/suites/device-onvif-camera/main_test.go
@@ -14,7 +14,7 @@ const (
 	deviceOnvifCameraApp         = "device-onvif-camera"
 	deviceOnvifcameraService     = deviceOnvifcameraSnap + "." + deviceOnvifCameraApp
 	deviceOnvifcameraServicePort = "59984"
-	testOnvifCredFile            = "/var/snap/edgex-device-onvif-camera/current/device-onvif-camera/onvif-credentials.json"
+	testOnvifCredFile            = "/var/snap/edgex-device-onvif-camera/current/config/device-onvif-camera/test-onvif-credentials.json"
 )
 
 func TestMain(m *testing.M) {
@@ -54,6 +54,7 @@ func TestMain(m *testing.M) {
 		return
 	}
 	err = os.WriteFile(testOnvifCredFile, testData, 0644)
+	err = os.WriteFile(testOnvifCredFile, testData, 0666)
 	if err != nil {
 		fmt.Println(err)
 		return
