55126303ca65df86962db85df7bfdf5e5fe96b69
Mengyi Wang <mengyi.wang@canonical.com>
2022-07-14 16:04:05 +0200

    Add device ONVIF camera test suite (#87)

---
+++ test/suites/device-onvif-camera/main_test.go
@@ -0,0 +1,81 @@
+package test
+
+import (
+	"edgex-snap-testing/test/utils"
+	"log"
+	"os"
+	"testing"
+	"time"
+)
+
+const (
+	deviceOnvifCameraSnap        = "edgex-device-onvif-camera"
+	deviceOnvifCameraApp         = "device-onvif-camera"
+	deviceOnvifCameraServicePort = "59984"
+)
+
+func TestMain(m *testing.M) {
+
+	log.Println("[SETUP]")
+	start := time.Now()
+	// start clean
+	utils.SnapRemove(nil,
+		deviceOnvifCameraSnap,
+		"edgexfoundry",
+	)
+
+	// install the device snap before edgexfoundry
+	// to catch build error sooner and stop
+	if utils.LocalSnap != "" {
+		utils.SnapInstallFromFile(nil, utils.LocalSnap)
+	} else {
+		utils.SnapInstallFromStore(nil, deviceOnvifCameraSnap, utils.ServiceChannel)
+	}
+	utils.SnapInstallFromStore(nil, "edgexfoundry", utils.PlatformChannel)
+
+	// make sure all services are online before starting the tests
+	utils.WaitPlatformOnline(nil)
+
+	// for local build, the interface isn't auto-connected.
+	// connect manually regardless
+	utils.SnapConnect(nil,
+		"edgexfoundry:edgex-secretstore-token",
+		deviceOnvifCameraSnap+":edgex-secretstore-token",
+	)
+
+	exitCode := m.Run()
+
+	log.Println("[TEARDOWN]")
+
+	utils.SnapDumpLogs(nil, start, deviceOnvifCameraSnap)
+
+	utils.SnapRemove(nil,
+		deviceOnvifCameraSnap,
+		"edgexfoundry",
+	)
+
+	os.Exit(exitCode)
+}
+
+func TestCommon(t *testing.T) {
+	utils.TestConfig(t, deviceOnvifCameraSnap, utils.Config{
+		TestChangePort: utils.ConfigChangePort{
+			App:                      deviceOnvifCameraApp,
+			DefaultPort:              deviceOnvifCameraServicePort,
+			TestLegacyEnvConfig:      utils.FullConfigTest,
+			TestAppConfig:            true,
+			TestGlobalConfig:         true,
+			TestMixedGlobalAppConfig: utils.FullConfigTest,
+		},
+	})
+
+	utils.TestNet(t, deviceOnvifCameraSnap, utils.Net{
+		StartSnap:        true,
+		TestOpenPorts:    []string{deviceOnvifCameraServicePort},
+		TestBindLoopback: []string{deviceOnvifCameraServicePort},
+	})
+
+	utils.TestPackaging(t, deviceOnvifCameraSnap, utils.Packaging{
+		TestSemanticSnapVersion: true,
+	})
+}
