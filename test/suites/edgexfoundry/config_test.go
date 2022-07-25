99f63bb5c1783d22417fc75c063526aaef41e30b
Mengyi Wang <mengyi.wang@canonical.com>
2022-07-25 14:35:52 +0200

    Add ChangeStartupMsg test for platform test
    resolves #78

--- test/suites/edgexfoundry/config_test.go
+++ test/suites/edgexfoundry/config_test.go
@@ -2,10 +2,13 @@ package test
 
 import (
 	"edgex-snap-testing/test/utils"
+	"github.com/stretchr/testify/require"
+	"strings"
 	"testing"
+	"time"
 )
 
-func TestEnvConfigChangePort(t *testing.T) {
+func TestChangePort_legacyEnv(t *testing.T) {
 	if !utils.FullConfigTest {
 		t.Skip("Full config test is disabled.")
 	}
@@ -33,4 +36,94 @@ func TestEnvConfigChangePort(t *testing.T) {
 	utils.SnapUnset(t, platformSnap, envServicePort)
 	utils.SnapRestart(t, supportSchedulerService)
 	utils.WaitServiceOnline(t, 60, supportSchedulerServicePort)
+}
+
+func TestChangeStartupMsg_global(t *testing.T) {
+	const (
+		newStartupMsg    = "snap-testing-startup-message"
+		configStartupMsg = "config.service-startupmsg"
+	)
+
+	// start clean
+	utils.SnapStop(t, supportSchedulerService)
+
+	t.Cleanup(func() {
+		utils.SnapUnset(t, platformSnap, configStartupMsg)
+		utils.SnapStop(t, supportSchedulerService)
+	})
+
+	// enable new config option to avoid mixed options issue with old env option
+	utils.SnapSet(t, platformSnap, "app-options", "true")
+
+	// make sure the startupmsg is not testing message before setting it
+	utils.SnapStart(t, supportSchedulerService)
+	require.False(t, checkStartupMsg(t, supportSchedulerService, newStartupMsg))
+
+	// set config. and validate the startupmsg is testing message
+	utils.SnapSet(t, platformSnap, configStartupMsg, newStartupMsg)
+	utils.SnapRestart(t, supportSchedulerService)
+	require.True(t, checkStartupMsg(t, supportSchedulerService, newStartupMsg))
+
+	// unset config. and validate the startupmsg is not testing message anymore
+	utils.SnapUnset(t, platformSnap, configStartupMsg)
+	utils.SnapRestart(t, supportSchedulerService)
+	require.False(t, checkStartupMsg(t, supportSchedulerService, newStartupMsg))
+}
+
+func TestChangeStartupMsg_mixedGlobalApp(t *testing.T) {
+	const (
+		defaultStartupMsg = "This is the Support Scheduler Microservice"
+		appNewStartupMsg  = "snap testing startup message (set by app option)"
+		appStartupMsg     = "apps." + supportSchedulerApp + ".config.service-startupmsg"
+
+		globalNewStartupMsg = "snap testing startup message (set by config option)"
+		globalStartupMsg    = "config.service-startupmsg"
+	)
+
+	// start clean
+	utils.SnapStop(t, supportSchedulerService)
+
+	t.Cleanup(func() {
+		utils.SnapUnset(t, platformSnap, globalStartupMsg)
+		utils.SnapStop(t, supportSchedulerService)
+	})
+
+	// enable new config option to avoid mixed options issue with old env option
+	utils.SnapSet(t, platformSnap, "app-options", "true")
+
+	// make sure the startupmsg is the default testing message before setting it
+	utils.SnapStart(t, supportSchedulerService)
+	require.True(t, checkStartupMsg(t, supportSchedulerService, defaultStartupMsg))
+
+	// set apps. and config. with different testing message,
+	// and validate that app-specific option has been picked up because it has higher precedence
+	utils.SnapSet(t, platformSnap, appStartupMsg, appNewStartupMsg)
+	utils.SnapSet(t, platformSnap, globalStartupMsg, globalNewStartupMsg)
+	utils.SnapRestart(t, supportSchedulerService)
+	require.True(t, checkStartupMsg(t, supportSchedulerService, appNewStartupMsg))
+
+	// unset apps. and config. and validate the startupmsg is back to default
+	utils.SnapUnset(t, platformSnap, appStartupMsg)
+	utils.SnapUnset(t, platformSnap, globalStartupMsg)
+	utils.SnapRestart(t, supportSchedulerService)
+	require.True(t, checkStartupMsg(t, supportSchedulerService, defaultStartupMsg))
+}
+
+func checkStartupMsg(t *testing.T, snap, expectedMsg string) bool {
+	const maxRetry = 10
+	var start = time.Now()
+
+	utils.WaitPlatformOnline(t)
+
+	for i := 1; i <= maxRetry; i++ {
+		time.Sleep(1 * time.Second)
+		t.Logf("Waiting for startup message. Retry %d/%d", i, maxRetry)
+
+		logs := utils.SnapLogs(t, start, snap)
+		if strings.Contains(logs, expectedMsg) {
+			return true
+		}
+	}
+	t.Logf("Time out: reached max %d retries.", maxRetry)
+	return false
 }
