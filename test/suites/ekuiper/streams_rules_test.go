5e5b1ebf4e438b2a6c261c173fcbb334e89561d0
Mengyi Wang <mengyi.wang@canonical.com>
2022-10-21 14:42:58 +0200

    update sink result test

--- test/suites/ekuiper/streams_rules_test.go
+++ test/suites/ekuiper/streams_rules_test.go
@@ -2,8 +2,19 @@ package test
 
 import (
 	"edgex-snap-testing/test/utils"
+	"encoding/json"
+	"fmt"
+	"github.com/stretchr/testify/require"
+	"io/ioutil"
+	"net/http"
+	"strings"
 	"testing"
+	"time"
 )
+
+type Reading struct {
+	TotalCount int `json:"totalCount"`
+}
 
 func TestStreamsAndRules(t *testing.T) {
 	t.Cleanup(func() {
@@ -56,10 +67,39 @@ func TestStreamsAndRules(t *testing.T) {
 	utils.TestDeviceVirtualReading(t)
 
 	t.Run("check rule_log", func(t *testing.T) {
-		utils.Exec(t, `edgex-ekuiper.kuiper-cli getstatus rule rule_log`)
+		//check logs for the record of expected log
+		start := time.Now()
+		time.Sleep(1 * time.Second)
+		logs := utils.SnapLogs(t, start, ekuiperSnap)
+		expectLog := "sink result for rule rule_log"
+
+		require.True(t, strings.Contains(logs, expectLog))
 	})
 
 	t.Run("check rule_edgex_message_bus", func(t *testing.T) {
-		utils.Exec(t, `edgex-ekuiper.kuiper-cli getstatus rule rule_edgex_message_bus`)
+		// wait device-virtual to come online and produce readings
+		utils.WaitServiceOnline(t, 60, deviceVirtualPort)
+
+		var reading Reading
+		resp, err := http.Get("http://localhost:59880/api/v2/reading/device/name/device-test")
+		if err != nil {
+			fmt.Print(err)
+			return
+		}
+		defer resp.Body.Close()
+
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Print(err)
+			return
+		}
+
+		err = json.Unmarshal(body, &reading)
+		if err != nil {
+			fmt.Print(err)
+			return
+		}
+
+		require.Greaterf(t, reading.TotalCount, 0, "No readings have been re-published to EdgeX message bus by ekuiper")
 	})
 }
