da58a4ad15930b5ead3cfd2eb5f9a3f4e06a7a68
Farshid Tavakolizadeh <farshid.tavakolizadeh@canonical.com>
2023-03-09 11:35:46 +0100

    Improve formatting

--- test/print-errors.sh
+++ test/print-errors.sh
@@ -4,6 +4,8 @@ suite=$1
 
 pattern="$(dirname "$0")/suites/$suite/*.log"
 for file in $pattern; do
-    echo -e "\nFiltered errors from $file"
+    echo -e "\n🟥 Filtered errors from $file:"
     cat $file | grep --ignore-case --extended-regexp --word-regexp "error|ERR"
 done
+
+echo -e "\n🔎 For full logs, refer to workflow artifacts."
