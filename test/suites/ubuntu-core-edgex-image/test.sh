cc9e2d742fa2faae440e5255d86486dbbe7036ff
Mengyi Wang <mengyi.wang@canonical.com>
2023-03-09 09:12:43 +0100

    [skip ci] Remove service status verification from outside

--- test/suites/ubuntu-core-edgex-image/test.sh
+++ test/suites/ubuntu-core-edgex-image/test.sh
@@ -8,23 +8,3 @@ do
   ssh "$USER@localhost" -p 8022 "curl -s http://localhost:$port/api/v2/ping; printf '\n'"
 done
 
-# # make core services listen to all interfaces
-# ssh "$USER@localhost" -p 8022 "snap set edgexfoundry app-options=true; printf '\n'"
-
-# services=(core-data core-metadata core-command)
-
-# for service in "${services[@]}"
-# do
-#   ssh "$USER@localhost" -p 8022 "snap set edgexfoundry apps.$service.config.service-serverbindaddr='0.0.0.0'; snap restart edgexfoundry.$service"
-# done
-
-# sleep 5
-
-
-# # check status of core and device services outside of the emulator
-# ports=(59880 59881 59882)
-# for port in "${ports[@]}"
-# do
-#   curl http://localhost:$port/api/v2/ping; printf '\n'
-# done
-
