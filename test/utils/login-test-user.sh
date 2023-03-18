25609e3b37343e36ba0fdc5daa7348c0354ee8f4
Farshid Tavakolizadeh <farshid.tavakolizadeh@canonical.com>
2023-03-18 17:13:53 +0100

    Add flag to make jq return non-zero exit code when not found
    Flag: -e / --exit-status

--- test/utils/login-test-user.sh
+++ test/utils/login-test-user.sh
@@ -3,8 +3,8 @@
 username=test
 password=$(sudo edgexfoundry.secrets-config proxy adduser --user ${username} --useRootToken | jq -r '.password')
 
-vault_token=$(curl -sS "http://localhost:8200/v1/auth/userpass/login/${username}" -d "{\"password\":\"${password}\"}" | jq -r '.auth.client_token')
+vault_token=$(curl -sS "http://localhost:8200/v1/auth/userpass/login/${username}" -d "{\"password\":\"${password}\"}" | jq -re '.auth.client_token')
 
-id_token=$(curl -sS -H "Authorization: Bearer ${vault_token}" "http://localhost:8200/v1/identity/oidc/token/${username}" | jq -r '.data.token')
+id_token=$(curl -sS -H "Authorization: Bearer ${vault_token}" "http://localhost:8200/v1/identity/oidc/token/${username}" | jq -re '.data.token')
 
 echo $id_token
