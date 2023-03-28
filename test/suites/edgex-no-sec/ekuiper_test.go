3ce2fe4d371b9065c76aada901bcf1d1986295c1
Mengyi Wang <mengyi.wang@canonical.com>
2023-03-28 16:29:37 +0200

    Move Device Virtual, ASC, eKuiper snaps to subtest

--- test/suites/edgex-no-sec/ekuiper_test.go
+++ test/suites/edgex-no-sec/ekuiper_test.go
@@ -3,9 +3,11 @@ package test
 import (
 	"edgex-snap-testing/test/utils"
 	"encoding/json"
-	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
 )
@@ -14,18 +16,26 @@ type Reading struct {
 	TotalCount int `json:"totalCount"`
 }
 
-func TestRulesEngine(t *testing.T) {
-	t.Cleanup(func() {
-		utils.SnapStop(t,
-			ekuiperSnap,
-			deviceVirtualSnap,
-			ascSnap)
-	})
+const (
+	deviceVirtualSnap = "edgex-device-virtual"
+	deviceVirtualApp  = "device-virtual"
+	deviceVirtualPort = "59900"
 
-	utils.SnapStart(t,
-		ekuiperSnap,
-		deviceVirtualSnap,
-		ascSnap)
+	ekuiperSnap           = "edgex-ekuiper"
+	ekuiperApp            = "ekuiper"
+	ekuiperServerPort     = "20498"
+	ekuiperRestfulApiPort = "59720"
+
+	ascSnap             = "edgex-app-service-configurable"
+	ascApp              = "app-service-configurable"
+	ascServiceRulesPort = "59701"
+)
+
+func TestRulesEngine(t *testing.T) {
+	teardown, err := subtestSetup(t)
+	if err != nil {
+		log.Fatalf("Failed to setup tests: %s", err)
+	}
 
 	t.Run("create stream and rule", func(t *testing.T) {
 		utils.Exec(t, `edgex-ekuiper.kuiper create stream stream1 '()WITH(FORMAT="JSON",TYPE="edgex")'`)
@@ -46,16 +56,17 @@ func TestRulesEngine(t *testing.T) {
 			   ]
 			}'`)
 
-		// wait device-virtual to come online and produce readings
-		utils.WaitServiceOnline(t, 60, deviceVirtualPort)
-		utils.WaitForReadings(t, false)
+		req, err := http.NewRequest(http.MethodGet, "http://localhost:59880/api/v2/reading/device/name/device-test", nil)
+		require.NoError(t, err)
+
+		idToken := utils.LoginTestUser(t)
+		req.Header.Set("Authorization", "Bearer "+idToken)
 
 		var reading Reading
-		resp, err := http.Get("http://localhost:59880/api/v2/reading/device/name/device-test")
-		if err != nil {
-			fmt.Print(err)
-			return
-		}
+		client := &http.Client{}
+		resp, err := client.Do(req)
+
+		require.NoError(t, err)
 		defer resp.Body.Close()
 
 		if err = json.NewDecoder(resp.Body).Decode(&reading); err != nil {
@@ -64,4 +75,106 @@ func TestRulesEngine(t *testing.T) {
 
 		require.Greaterf(t, reading.TotalCount, 0, "No readings have been re-published to EdgeX message bus by ekuiper")
 	})
+
+	teardown()
+}
+
+func subtestSetup(t *testing.T) (teardown func(), err error) {
+	log.Println("[SUBTEST CLEAN]")
+	utils.SnapRemove(t, deviceVirtualSnap)
+	utils.SnapRemove(t, ekuiperSnap)
+	utils.SnapRemove(t, ascSnap)
+
+	log.Println("[SUBTEST SETUP]")
+	start := time.Now()
+
+	teardown = func() {
+		log.Println("[SUBTEST TEARDOWN]")
+		utils.SnapDumpLogs(t, start, deviceVirtualSnap)
+		utils.SnapDumpLogs(t, start, ekuiperSnap)
+		utils.SnapDumpLogs(t, start, ascSnap)
+
+		log.Println("Removing installed snap:", !utils.SkipTeardownRemoval)
+		if !utils.SkipTeardownRemoval {
+			utils.SnapRemove(t, deviceVirtualSnap)
+			utils.SnapRemove(t, ekuiperSnap)
+			utils.SnapRemove(t, ascSnap)
+		}
+	}
+
+	if err = utils.SnapInstallFromStore(t, deviceVirtualSnap, utils.ServiceChannel); err != nil {
+		teardown()
+		return
+	}
+
+	if err = utils.SnapInstallFromStore(t, ekuiperSnap, utils.ServiceChannel); err != nil {
+		teardown()
+		return
+	}
+
+	if err = utils.SnapInstallFromStore(t, ascSnap, utils.ServiceChannel); err != nil {
+		teardown()
+		return
+	}
+
+	// turn security off
+	utils.SnapSet(t, deviceVirtualSnap, "config.edgex-security-secret-store", "false")
+	utils.SnapSet(t, ascSnap, "config.edgex-security-secret-store", "false")
+	utils.SnapSet(t, ekuiperSnap, "config.edgex-security-secret-store", "false")
+
+	// use ASC for event filtering
+	utils.SnapSet(t, ekuiperSnap, "config.edgex.default.topic", "rules-events")
+	utils.SnapSet(t, ekuiperSnap, "config.edgex.default.messagetype", "event")
+	utils.SnapSet(t, ascSnap, "profile", "rules-engine")
+
+	// set tests to run without a config provider when testing config options as a temporary solution. 
+	// update this once the following PR has been merged: https://github.com/canonical/edgex-snap-testing/pull/175
+	disableConfigProviderServiceSnap(t, deviceVirtualSnap, deviceVirtualApp)
+	disableConfigProviderServiceSnap(t, ascSnap, ascApp)
+
+	// make sure all services are online before starting the tests
+	utils.SnapStart(t,
+		ekuiperSnap,
+		deviceVirtualSnap,
+		ascSnap)
+
+	if err = utils.WaitServiceOnline(t, 60,
+		deviceVirtualPort,
+		ekuiperServerPort,
+		ekuiperRestfulApiPort,
+		ascServiceRulesPort,
+	); err != nil {
+		teardown()
+		return
+	}
+
+	// wait device-virtual to produce readings
+	utils.WaitForReadings(t, false)
+
+	return
+}
+
+// disableConfigProviderServiceSnap disables the config provider for the specified app,
+// copies the common configuration file from the platform snap to the service snap,
+// and sets the common configuration path.
+func disableConfigProviderServiceSnap(t *testing.T, snap, app string) {
+	utils.SnapSet(t, snap, "apps."+app+".config.edgex-config-provider", "none")
+
+	t.Logf("Copying coommon config file from platform snap to service snap: %s", snap)
+
+	sourceFile := "/snap/edgexfoundry/current/config/core-common-config-bootstrapper/res/configuration.yaml"
+	destFile := "/var/snap/" + snap + "/current/config/common-config.yaml"
+	// read the source common config file
+	source, err := ioutil.ReadFile(sourceFile)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// write the source file contents to the destination file
+	err = ioutil.WriteFile(destFile, source, 0644)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	utils.SnapSet(t, snap, "apps."+app+".config.edgex-common-config", destFile)
 }
