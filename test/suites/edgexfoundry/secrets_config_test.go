b86232512d4115968283cd942b07ea12269c8e65
Mengyi Wang <mengyi.wang@canonical.com>
2023-05-10 15:00:08 +0200

    Remove sleep time

--- test/suites/edgexfoundry/secrets_config_test.go
+++ test/suites/edgexfoundry/secrets_config_test.go
@@ -59,9 +59,8 @@ func TestChangeTLSCert(t *testing.T) {
 	createTLSCert(t)
 
 	t.Log("Calling API gateway using new TLS certificates:", coreDataPingEndpoint)
-	time.Sleep(5 * time.Second)
-
 	const caCertFile = tmpDir + "/ca.cert"
+
 	// Note: %%	is a literal percent sign
 	// Note: The ca.cert file is created by create-tls-certificates.sh
 	code, _, _ := utils.Exec(t, fmt.Sprintf(
